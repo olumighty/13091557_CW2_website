60c47ab1fb010e531f64b7786bc6e0ee7f9fd476
olumighty <olushowems@gmail.com>
2017-04-28 10:33:18 +0100

    updated reg file

--- customer_register.php
+++ customer_register.php
@@ -24,7 +24,52 @@ include("toppage/bannernav.php");
 ?>
 	
 		<!--Content wrapper starts-->
+		<div class="content_wrapper">
 		
+			<div id="sidebar">
+			
+				<div id="sidebar_title">Sizes</div>
+				
+				<ul id="cats">
+				
+				<?php getCats(); ?>
+				
+				<ul>
+					
+				<div id="sidebar_title">Location</div>
+				
+				<ul id="cats">
+					
+					<?php getBrands(); ?>
+				
+				<ul>
+			
+			
+			</div>
+		
+			<div id="content_area">
+			
+			<?php cart(); ?>
+			
+			<div id="shopping_cart"> 
+					
+					<span style="float:right; font-size:18px; padding:5px; line-height:40px;">
+					
+					Welcome!<a href="cart.php" style="color:yellow">Go to Cart</a>
+					
+					
+					
+					</span>
+			</div>
+			
+				<form action="customer_register.php" method="post" enctype="multipart/form-data">
+					
+					
+				
+				</form>
+			
+			</div>
+		</div>
 		<!--Content wrapper ends-->
 		
 		
