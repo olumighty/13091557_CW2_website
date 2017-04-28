581bac42a070c0ab23cd5fc2ea8fe3a2c9373c3f
olumighty <olushowems@gmail.com>
2017-04-28 09:55:47 +0100

    initial reg file

---
+++ customer_register.php
@@ -0,0 +1,54 @@
+<!DOCTYPE>
+<?php 
+session_start();
+include("functions/functions.php");
+include("includes/db.php"); 
+?>
+<html>
+	<head>
+		<title>Agency</title>
+		
+		
+	<link rel="stylesheet" href="styles/style.css" media="all" /> 
+	</head>
+	
+<body>
+	
+	<!--Main Container starts here-->
+	<div class="main_wrapper">
+	
+		<?php 
+
+include("toppage/bannernav.php");
+
+?>
+	
+		<!--Content wrapper starts-->
+		
+		<!--Content wrapper ends-->
+		
+		
+		
+				<?php 
+
+include("footer/footer.php");
+
+?>
+	
+	</div> 
+<!--Main Container ends here-->
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
