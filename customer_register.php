d4c20cc37b43a23ae3724bea6df3833bcbd68900
olumighty <olushowems@gmail.com>
2017-04-28 10:55:31 +0100

    second updated with form elements reg file

--- customer_register.php
+++ customer_register.php
@@ -64,7 +64,73 @@ include("toppage/bannernav.php");
 			
 				<form action="customer_register.php" method="post" enctype="multipart/form-data">
 					
+					<table align="center" width="750">
+						
+						<tr align="center">
+							<td colspan="6"><h2>Create an Account</h2></td>
+						</tr>
+						
+						<tr>
+							<td align="right">Customer Name:</td>
+							<td><input type="text" name="c_name" required/></td>
+						</tr>
+						
+						<tr>
+							<td align="right">Customer Email:</td>
+							<td><input type="text" name="c_email" required/></td>
+						</tr>
+						
+						<tr>
+							<td align="right">Customer Password:</td>
+							<td><input type="password" name="c_pass" required/></td>
+						</tr>
+						
+						
+						
+						
+						
+						<tr>
+							<td align="right">Customer Country:</td>
+							<td>
+							<select name="c_country">
+								<option>Select a Country</option>
+								<option>Afghanistan</option>
+								<option>India</option>
+								<option>Japan</option>
+								<option>Pakistan</option>
+								<option>Israel</option>
+								<option>Nepal</option>
+								<option>United Arab Emirates</option>
+								<option>United States</option>
+								<option>United Kingdom</option>
+							</select>
+							
+							</td>
+						</tr>
+						
+						<tr>
+							<td align="right">Customer City:</td>
+							<td><input type="text" name="c_city" required/></td>
+						</tr>
+						
+						<tr>
+							<td align="right">Customer Contact:</td>
+							<td><input type="text" name="c_contact" required/></td>
+						</tr>
+						
+						<tr>
+							<td align="right">Customer Address</td>
+							<td><input type="text" name="c_address" required/></td>
+						</tr>
+						
+						
+					<tr align="center">
+						<td colspan="6"><input type="submit" name="register" value="Create Account" /></td>
+					</tr>
 					
+					
+					
+					</table>
 				
 				</form>
 			
